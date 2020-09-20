8298a48a5a1af2bd618377ab9af2e9124b1ac568
Hung <mhungict99@gmail.com>
2020-09-20 19:30:32 +0700

    connect to db , add detail courses

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -2,8 +2,8 @@ const express = require('express');
 const route = require('express');
 const router = express.Router();
 
-const newsController = require('../app/controllers/NewsController');
+const courseController = require('../app/controllers/CourseController');
 
-router.get('/', newsController.index);
+router.get('/:slug', courseController.show);
 
 module.exports = router;
